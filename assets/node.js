e3ce117020657be2f5808df8fdc303eb2b52ba85
johnnywang <johnny.cy.wang@aaa.com>
2024-02-21 23:46:19 +0800

    UPD: add gear head

--- assets/node.js
+++ assets/node.js
@@ -1,6 +1,15 @@
 const fs = require('fs')
+const { exec } = require('node:child_process');
 
-const folder = './live2d/sin'
+const folder = './spine/ark_recode/head'
 
 const res = fs.readdirSync(folder)
-console.log(JSON.stringify(res, null, 2))
+// console.log(JSON.stringify(res, null, 2))
+
+const result = res.map((name) => {
+  const matched = name.match(/^Icon_Head_(L|S)_/g);
+  console.log(matched);
+  exec(`mv ${folder}/${name} ${folder}/${name.replace(matched, '')}`);
+  return name.replace(matched, '');
+})
+console.log(JSON.stringify(result, null, 2));
